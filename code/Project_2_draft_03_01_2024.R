d94a0847df49ccf6205755507d916fdb428b581e
IzzyBedic <izzy@bedichek.org>
2024-03-22 15:43:54 -0400

    decided on variables to use in analysis

--- code/Project_2_draft_03_01_2024.R
+++ code/Project_2_draft_03_01_2024.R
@@ -25,40 +25,47 @@ library(tidymodels)
 
 pine_tbl <- read_excel("../hgen-612_temp/p1/data/Data_1993.xlsx", sheet = 1)
 
-
 # Showcasing the Data
 
 ggpairs(pine_tbl, columns = c("DeadDist", 
                               "TreeDiam", 
                               "Infest_Serv1", 
-                              "SDI_20th")) 
+                              "SDI_20th",
+                              "BA_Inf_20th")) 
 
-ggpairs(pine_tbl, columns = c("DeadDist", 
-                              "Ind_DeadDist")) # Only need one of these bc highly corr
+
+# ggpairs(pine_tbl, columns = c("DeadDist", 
+#                               "Ind_DeadDist")) # DeadDist is given in homework desc
 
 ggpairs(pine_tbl, columns = c("SDI_20th", 
-                              "Neigh_SDI_1/4th", 
-                              "BA_20th", 
-                              "Neigh_1/4th", 
-                              "Neigh_1/2th",
-                              "Neigh_1",
-                              "Neigh_1.5")) # multicollinearity, only need one
+                            # "Neigh_SDI_1/4th", 1/20th pref
+                              "BA_20th")) # comes down to SDI or basal area, multicollinearity. Decided on SDI!
+                            #  "Neigh_1/4th", 1/20th pref
+                            #  "Neigh_1/2th", 1/20th pref
+                            #  "Neigh_1")), 1/20th pref
+                            # "Neigh_1.5")) smaller acreage always 
+                            # multicollinearity, only need one
 
-ggpairs(pine_tbl, columns = c("BA_Inf_20th",
-                              "BA_Infest_1/4th",
-                              "BA_Infest_1/2th",
-                              "BA_Infest_1",
-                              "BA_Infest_1.5")) # multicollinearity, only need one
+# ggpairs(pine_tbl, columns = c("BA_Inf_20th", --> winner, closest to pine beetle range
+                              # "BA_Infest_1/4th", 1/20th pref
+                              # "BA_Infest_1/2th", 1/20th pref
+                              # "BA_Infest_1", 1/20th pref
+                              # "BA_Infest_1.5")) 1/20th pref
 
-ggpairs(pine_tbl, columns = c("IND_BA_Infest_20th",
-                              "IND_BA_Infest_1/4th",
-                              "IND_BA_Infest_1/2th",
-                              "IND_BA_Infest_1",
-                              "IND_BA_Infest_1.5")) # multicollinearity, only need one
+# ggpairs(pine_tbl, columns = c("IND_BA_Infest_20th", --> winner
+                             # "IND_BA_Infest_1/4th", 1/20th pref
+                             # "IND_BA_Infest_1/2th", 1/20th pref
+                             # "IND_BA_Infest_1", 1/20th pref
+                             # "IND_BA_Infest_1.5")) 1/20th pref
 
+# how to compare "IND_BA_Infest_20th" and "BA_Inf_20th"? 
+# Binary is harder to see a normal dist for, so I'll use the non-binary one
+        
+        
 # Haven't decided which variables to use, but here is the code that will be used
-
+# Use 1/20th when possible, and factor in TreeDiam
 # Create recipe
+
 pine_recipe <- pine_tbl %>% 
   recipe(DeadDist ~ TreeDiam + Infest_Serv1 +  SDI_20th + BA_20th) %>% 
   step_sqrt(all_outcomes()) %>% # transforms to make normal --- MAKE SURE STILL SHOULD BE SQRT IF NEW VARIABLES
